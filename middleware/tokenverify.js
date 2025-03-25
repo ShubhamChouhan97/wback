15471bb2ba27836502226c0de23a1daff59a6476
ShubhamChouhan97 <shubhamsc9799@gmail.com>
2025-03-25 15:34:28 +0530

    tokenverify set

--- middleware/tokenverify.js
+++ middleware/tokenverify.js
@@ -1,31 +1,11 @@
-// const jwt = require('jsonwebtoken');
-
-// // Authentication Middleware
-// const authenticate = (req, res, next) => {
-//     const token = req.cookies.token; // Extract token from cookies
-//     console.log("auth call at tokken middleware");
-//     if (!token) {
-//       return res.status(401).json({ message: "Unauthorized" }); // No token found
-//     }
-  
-//     try {
-//       const decoded = jwt.verify(token, process.env.JWT_SECRET); // Verify token
-//       req.user = decoded; // Attach decoded user info to request
-//       next(); // Proceed to the next middleware/controller
-//     } catch (error) {
-//       return res.status(401).json({ message: "Invalid token" }); // Token verification failed
-//     }
-//   };
-  
-//   module.exports = authenticate;
 const jwt = require('jsonwebtoken');
-
+const User = require("../models/User");
 // Authentication Middleware
-const tokenverify = (req, res, next) => {
+const tokenverify = async (req, res) => {
   // Extract token from cookies
   const token = req.cookies.token; 
   const email = req.body.user;
-  //console.log("Auth middleware called. Token:", token);
+  //console.log("Auth middleware called. at tokenverify");
 
   // Check if token is missing
   if (!token) {
@@ -35,18 +15,20 @@ const tokenverify = (req, res, next) => {
   try {
     // Verify the token
     const decoded = jwt.verify(token, process.env.JWT_SECRET); 
+    const user = await User.findOne({ email });
+   // console.log("user token",user.token);
+   // console.log("token",token);
+
+    if(user.token === token)
+    {
+          return res.status(200).json({ message: "authorized" });
+
+    }
+    else{
+      return res.status(401).json({ message: "Unauthorized" });
+    }
     //console.log("DDDD",decoded);
-    // Attach decoded user info to request
     
-if(email === decoded.email)
-{
-//console.log("tokken verify check ");
-    return res.status(200).json({ message: "authorized" });
-}
-else{
-    return res.status(401).json({ message: "Unauthorized" });
-}
-    // Proceed to the next middleware/controller
     
   } catch (error) {
    // console.error("Token verification failed:", error);
